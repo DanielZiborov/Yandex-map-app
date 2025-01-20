87c027ba24a58f7bd518edd5fdd8ba162781716a
DanielZiborov <ziborovdanila5@gmail.com>
2025-01-20 20:01:41 +0300

    change MainActivity.kt

--- android/app/src/main/kotlin/com/example/yandex_map_application/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yandex_map_application/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.yandex_map_application
 
+import androidx.annotation.NonNull
+
 import io.flutter.embedding.android.FlutterActivity
-
-import android.app.Application
-
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 import com.yandex.mapkit.MapKitFactory
 
-class MainApplication: Application() {
-  override fun onCreate() {
-    super.onCreate()
-    MapKitFactory.setLocale("YOUR_LOCALE") // Your preferred language. Not required, defaults to system language
-    MapKitFactory.setApiKey("20b56f2e-29c8-4fd1-a2a3-bce52cfeb186") // Your generated API key
-  }
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("20b56f2e-29c8-4fd1-a2a3-bce52cfeb186")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
