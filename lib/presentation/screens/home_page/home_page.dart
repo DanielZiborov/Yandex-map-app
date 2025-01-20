d9fd025a0470dfdbe074c4cd0b5f737deb03a97c
DanielZiborov <ziborovdanila5@gmail.com>
2025-01-20 21:27:58 +0300

    commit3

--- lib/presentation/screens/home_page/home_page.dart
+++ lib/presentation/screens/home_page/home_page.dart
@@ -1,10 +1,79 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:yandex_map_application/core/services/map_services/location_service.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final mapControllerCompleter = Completer<YandexMapController>();
+
+  @override
+  void initState() {
+    super.initState();
+    _initPermission().ignore();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Stack(
+        children: [
+          YandexMap(
+            onMapCreated: (controller) =>
+                mapControllerCompleter.complete(controller),
+          ),
+          Align(
+            child: ElevatedButton(
+              style: const ButtonStyle(shape: WidgetStatePropertyAll(BeveledRectangleBorder())),
+              onPressed: (){},
+              child: const Text("asdsfsdfsd"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _initPermission() async {
+    if (!await LocationService().checkPermission()) {
+      await LocationService().requestPermission();
+    }
+    await _fetchCurrentLocation();
+  }
+
+  Future<void> _fetchCurrentLocation() async {
+    AppLatLong location;
+    const defLocation = SidneyLocation();
+    try {
+      location = await LocationService().getCurrentLocation();
+    } catch (_) {
+      location = defLocation;
+    }
+    _moveToCurrentLocation(location);
+  }
+
+  Future<void> _moveToCurrentLocation(
+    AppLatLong appLatLong,
+  ) async {
+    (await mapControllerCompleter.future).moveCamera(
+      animation: const MapAnimation(type: MapAnimationType.linear, duration: 1),
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: Point(
+            latitude: appLatLong.lat,
+            longitude: appLatLong.long,
+          ),
+          zoom: 12,
+        ),
+      ),
+    );
   }
 }
